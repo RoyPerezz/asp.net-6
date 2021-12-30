5671f3b101fa853ac0401fb41e4b157de8f9d34d
RoyPerezz <royprez@gmail.com>
2021-12-30 17:49:47 -0600

    Cadena de conexion
    este primer commit es para mostrar el error que me aparece en mi proyecto

---
+++ ManejoPresupuesto/Models/TipoCuenta.cs
@@ -0,0 +1,31 @@
+﻿using ManejoPresupuesto.Validaciones;
+using System.ComponentModel.DataAnnotations;
+
+namespace ManejoPresupuesto.Models
+{
+    public class TipoCuenta
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage ="El {0} es requerido")]
+
+        [Display(Name ="Nombre del tipo de Cuenta")]
+
+        [PrimeraLetraMayuscula]
+        public string Nombre { get; set; }
+        public int UsuarioId { get; set; }
+        public int Orden { get; set; }
+
+        //public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        //{
+        //    if (Nombre !=null && Nombre.Length>0)
+        //    {
+        //        var primeraLetra = Nombre[0].ToString();
+
+        //        if (primeraLetra !=primeraLetra.ToUpper())
+        //        {
+        //            yield return new ValidationResult("La primera letra debe ser mayuscula",new[] {nameof(Nombre) });
+        //        }
+        //    }
+        //}
+    }
+}
