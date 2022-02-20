07fb63cccca622731449a99180593e338318f957
RoyPerezz <royprez@gmail.com>
2022-02-19 19:32:18 -0600

    nuevos cambios

--- ManejoPresupuesto/Servicios/RepositorioCuentas.cs
+++ ManejoPresupuesto/Servicios/RepositorioCuentas.cs
@@ -8,8 +8,11 @@ namespace ManejoPresupuesto.Servicios
 
     public interface IRepositorioCuentas
     {
+        Task Actualizar(CuentaCreacionViewModel cuenta);
+        Task Borrar(int id);
         Task<IEnumerable<Cuenta>> Buscar(int usuarioId);
         Task Crear(Cuenta cuenta);
+        Task<Cuenta> ObtenerPorId(int id, int usuarioId);
     }
     public class RepositorioCuentas:IRepositorioCuentas
     {
@@ -34,8 +37,32 @@ namespace ManejoPresupuesto.Servicios
         public async Task<IEnumerable<Cuenta>> Buscar(int usuarioId)
         {
             using var connection = new SqlConnection(connectionString);
-            return await connection.QueryAsync<Cuenta>(@"SELECT Cuentas.Id,Cuentas.Nombre,Balance,tc.Nombre as TipoCuenta FROM Cuentas INNER JOIN TiposCuentas tc ON
-             tc.Id = Cuentas.TipoCuentaId WHERE tc.UsuarioId = @UsuarioId ORDER BY tc.Orden", new {usuarioId });
+            return await connection.QueryAsync<Cuenta>(@"SELECT Cuentas.Id,Cuentas.Nombre,Balance,tc.Nombre as TipoCuenta 
+                                                        FROM Cuentas 
+                                                        INNER JOIN TiposCuentas tc ON tc.Id = Cuentas.TipoCuentaId 
+                                                        WHERE tc.UsuarioId = @UsuarioId ORDER BY tc.Orden", new {usuarioId });
+        }
+
+        public async Task<Cuenta>ObtenerPorId(int id, int usuarioId)
+        {
+            using var connection = new SqlConnection(connectionString);
+            return await connection.QueryFirstOrDefaultAsync<Cuenta>(@"SELECT Cuentas.Id,Cuentas.Nombre,Balance,Descripcion,tc.Id
+                                                                        FROM Cuentas 
+                                                                        INNER JOIN TiposCuentas tc ON tc.Id = Cuentas.TipoCuentaId WHERE tc.UsuarioId = @UsuarioId and Cuentas.Id=@Id", new { id,usuarioId});
+        }
+
+        public async Task Actualizar(CuentaCreacionViewModel cuenta)
+        {
+            using var connection = new SqlConnection(connectionString);
+            await connection.ExecuteAsync(@"UPDATE Cuentas 
+                                            SET Nombre=@Nombre,Balance=@Balance,Descripcion=@Descripcion,TipoCuentaId=@TipoCuentaId 
+                                            WHERE Id=@Id;",cuenta);
+        }
+
+        public async Task Borrar(int id)
+        {
+            using var connection = new SqlConnection(connectionString);
+            await connection.ExecuteAsync("Delete Cuentas where Id=@Id", new { id});
         }
     }
 }
